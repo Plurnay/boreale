36f1967dba1af95eac5d1d339d6ff7e5f329050b
Paul-Rene Hebert <hebert.paulrene@jlitconsulting.ca>
2015-05-08 09:52:37 -0300

    added all files

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-05-05 23:39:30 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-05-05T23:39:30+00:00	INFO 192.168.0.10	joomlafailure	Mot de passe invalide
+2015-05-05T23:57:20+00:00	INFO 192.168.0.10	joomlafailure	Un mot de passe vide n'est pas autorisé
